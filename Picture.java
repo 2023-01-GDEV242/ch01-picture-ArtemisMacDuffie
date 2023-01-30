25e8535a814a1fce28cb0837c8dc05288d084312
ArtemisMacDuffie <G00168172@stu.raritanval.edu>
2023-01-30 12:01:01 -0500

    color draw

--- Picture.java
+++ Picture.java
@@ -30,16 +30,16 @@ public class Picture
     public Picture()
     {
         engine = new Square();
-        bed1 = new Square();
-        bed2 = new Square();
         cabin = new Triangle();
         cabinFloor1 = new Square();
         cabinFloor2 = new Square();
-        window = new Triangle();
-        wheel1 = new Circle();
-        wheel2 = new Circle();
         pass1 = new Person();
         pass2 = new Person();
+        window = new Triangle();
+        bed1 = new Square();
+        bed2 = new Square();
+        wheel1 = new Circle();
+        wheel2 = new Circle();
         drawn = false;
     }
 
@@ -63,8 +63,50 @@ public class Picture
             
             cabinFloor1.changeColor("red");
             cabinFloor1.moveHorizontal(-175);
-            cabinFloor1.moveVertical(75);
+            cabinFloor1.moveVertical(70);
             cabinFloor1.makeVisible();
+            
+            cabinFloor2.changeColor("red");
+            cabinFloor2.moveHorizontal(-120);
+            cabinFloor2.moveVertical(70);
+            cabinFloor2.makeVisible();
+            
+            pass1.changeSize(100, 50);
+            pass1.moveVertical(-45);
+            pass1.makeVisible();
+            
+            pass2.changeSize(100, 50);
+            pass2.moveHorizontal(75);
+            pass2.moveVertical(-45);
+            pass2.makeVisible();
+            
+            window.changeColor("blue");
+            window.changeSize(65,125);
+            window.moveHorizontal(-25);
+            window.moveVertical(-35);
+            window.makeVisible();
+            
+            bed1.changeColor("red");
+            bed1.moveHorizontal(-60);
+            bed1.moveVertical(40);
+            bed1.changeSize(100);
+            bed1.makeVisible();
+            
+            bed2.changeColor("red");
+            bed2.moveHorizontal(20);
+            bed2.moveVertical(40);
+            bed2.changeSize(100);
+            bed2.makeVisible();
+            
+            wheel1.changeColor("black");
+            wheel1.moveHorizontal(-125);
+            wheel1.moveVertical(125);
+            wheel1.makeVisible();
+            
+            wheel2.changeColor("black");
+            wheel2.moveHorizontal(110);
+            wheel2.moveVertical(125);            
+            wheel2.makeVisible();
             
             drawn = true;
         }
